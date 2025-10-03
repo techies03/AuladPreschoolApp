64c1d4df4989b8a325cf36db045c7da3cf0eed54
Ceed03 <mrmuqsit2003@gmail.com>
2025-10-03 15:16:19 +0800

    test

--- src/main/java/com/aulad/AuladPreschoolApp/App.java
+++ src/main/java/com/aulad/AuladPreschoolApp/App.java
@@ -22,6 +22,7 @@ public class App extends Application {
 		stage.centerOnScreen();
 		stage.getIcons().add(new Image(getClass().getResourceAsStream("/auladLogo.png")));
 		stage.show();
+		//
 	}
 
 	public static void main(String[] args) {
